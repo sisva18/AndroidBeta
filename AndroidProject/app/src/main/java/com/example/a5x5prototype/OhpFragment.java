35b01b90d1a73db0707f64a042d8428f2813016f
Gandrup <43139552+AGandrup@users.noreply.github.com>
2020-05-24 21:09:25 +0200

    fix for new fragments

--- AndroidProject/app/src/main/java/com/example/a5x5prototype/OhpFragment.java
+++ AndroidProject/app/src/main/java/com/example/a5x5prototype/OhpFragment.java
@@ -2,63 +2,163 @@ package com.example.a5x5prototype;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OhpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OhpFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private TextView txtView, txtFeedBack;
+    private Button setOne, setTwo, setThree, setFour, setFive;
+
+    private boolean setOneCompleted = false;
+    private boolean setTwoCompleted = false;
+    private boolean setThreeCompleted = false;
+    private boolean setFourCompleted = false;
+    private boolean setFiveCompleted = false;
+
+    public boolean dataWasSet = false;
 
     public OhpFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OhpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OhpFragment newInstance(String param1, String param2) {
-        OhpFragment fragment = new OhpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ohp, container, false);
+        DatabaseHelper db = new DatabaseHelper(getActivity());
+        View view = inflater.inflate(R.layout.fragment_ohp, container, false);
+        txtView = view.findViewById(R.id.txt_Squat);
+        txtFeedBack=view.findViewById(R.id.txt_feedBack);
+        txtView.setText("Overhead press: 5x5: "+db.getLastEntry("OHP", "name")*0.8+"KG");
+
+        setOne = view.findViewById(R.id.buttonSet1);
+        setTwo=view.findViewById(R.id.buttonSet2);
+        setThree=view.findViewById(R.id.buttonSet3);
+        setFour=view.findViewById(R.id.buttonSet4);
+        setFive=view.findViewById(R.id.buttonSet5);
+
+        setOneCompleted = false;
+        setTwoCompleted = false;
+        setThreeCompleted = false;
+        setFourCompleted = false;
+        setFiveCompleted = false;
+
+        setOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!setTwoCompleted) {
+                    if (!setOneCompleted) {
+                        // setOne.setVisibility(View.);
+                        setOne.setBackgroundResource(R.drawable.success);
+                        txtFeedBack.setText("Congratulations, complete the next 4 sets to increment max!");
+                        setOneCompleted = true;
+                    } else {
+                        setOne.setBackgroundResource(R.drawable.fail);
+                        setTwo.setBackgroundResource(R.drawable.n2);
+                        txtFeedBack.setText("Lift failed, try again next time!");
+                        setOneCompleted = false;
+                    }
+                }
+            }
+        });
+
+        setTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (setOneCompleted && !setThreeCompleted) {
+                    if (!setTwoCompleted) {
+                        // setOne.setVisibility(View.);
+                        setTwo.setBackgroundResource(R.drawable.success);
+                        txtFeedBack.setText("Congratulations, complete the next 3 sets to increment max!");
+                        setTwoCompleted = true;
+                    } else {
+                        setTwo.setBackgroundResource(R.drawable.fail);
+                        txtFeedBack.setText("Lift failed, try again next time!");
+                        setThree.setBackgroundResource(R.drawable.n3);
+                        setTwoCompleted = false;
+                    }
+                }
+            }
+        });
+
+        setThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (setTwoCompleted && !setFourCompleted) {
+                    if (!setThreeCompleted) {
+                        // setOne.setVisibility(View.);
+                        setThree.setBackgroundResource(R.drawable.success);
+                        txtFeedBack.setText("Congratulations, complete the next 2 sets to increment max!");
+                        setThreeCompleted = true;
+                    } else {
+                        setThree.setBackgroundResource(R.drawable.fail);
+                        txtFeedBack.setText("Lift failed, try again next time!");
+                        setFour.setBackgroundResource(R.drawable.n4);
+                        setThreeCompleted = false;
+                    }
+                }
+            }
+        });
+
+        setFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (setThreeCompleted && ! setFiveCompleted) {
+                    if (!setFourCompleted) {
+                        // setOne.setVisibility(View.);
+                        setFour.setBackgroundResource(R.drawable.success);
+                        txtFeedBack.setText("Congratulations, complete the next 1 sets to increment max!");
+                        setFourCompleted = true;
+                    } else {
+                        setFour.setBackgroundResource(R.drawable.fail);
+                        txtFeedBack.setText("Lift failed, try again next time!");
+                        setFive.setBackgroundResource(R.drawable.n5);
+                        setFourCompleted = false;
+                    }
+                }
+            }
+        });
+
+        setFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (setFourCompleted) {
+                    if (!setFiveCompleted) {
+                        // setOne.setVisibility(View.);
+                        setFive.setBackgroundResource(R.drawable.success);
+                        txtFeedBack.setText("Congratulations, overhead press max increment 2.5Kg");
+                        db.UpdateLatestMax(db.getLastEntry("OHP", "name")+2.5, "OHP", "name");
+                        setFiveCompleted = true;
+                    } else {
+                        setFive.setBackgroundResource(R.drawable.fail);
+                        txtFeedBack.setText("Lift failed, try again next time!");
+                        db.UpdateLatestMax(db.getLastEntry("OHP", "name"), "OHP", "name");
+                        setFiveCompleted = false;
+                    }
+                }
+
+                if(dataWasSet){
+                    setOne.setEnabled(false);
+                    setTwo.setEnabled(false);
+                    setThree.setEnabled(false);
+                    setFour.setEnabled(false);
+                    setFive.setEnabled(false);
+                }
+            }
+        });
+        return view;
     }
 }
