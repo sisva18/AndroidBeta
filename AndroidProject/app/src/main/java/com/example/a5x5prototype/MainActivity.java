de0856ebbd575c34a64a169ce4db03a66ff1c9e9
Gandrup <43139552+AGandrup@users.noreply.github.com>
2020-05-30 11:30:51 +0200

    deleted unused files

--- AndroidProject/app/src/main/java/com/example/a5x5prototype/MainActivity.java
+++ AndroidProject/app/src/main/java/com/example/a5x5prototype/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.a5x5prototype;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.view.View;
-import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
 
     private TextView workOut, maxes, firstTime;
